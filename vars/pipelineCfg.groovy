7e6f94940a53586828b547c3b9caeebfc13b7c1f
lisongtao <lisongtao@hrfax.cn>
2024-05-25 00:33:46 +0800

    init

--- vars/pipelineCfg.groovy
+++ vars/pipelineCfg.groovy
@@ -6,8 +6,17 @@ def call() {
 
     // 判断Map中是否存在include字段
     if (pipelineCfg.containsKey('include')) {
-        // 如果存在，读取include字段对应的YAML文件
+        // 如果存在，读取include字段的值
         String includeFile = pipelineCfg.include
+
+        // 判断includeFile是否带路径（假设带路径的文件名包含'/'或'\'）
+        if (!includeFile.contains('/')) {
+            // 如果不带路径，从JOB_NAME中提取前面的路径，并拼接到includeFile前面
+            String jobPath = JOB_NAME.substring(0, JOB_NAME.lastIndexOf('/') + 1)
+            includeFile = jobPath + includeFile
+        }
+
+        // 读取includeFile对应的YAML文件
         Map includeCfg = readYaml file: includeFile
 
         // 检查并合并两个Map对象
