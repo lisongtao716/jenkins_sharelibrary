8cb30b8bd883535ef7394f97ff8098ad4c7d8db9
lisongtao <lisongtao@hrfax.cn>
2024-05-24 22:10:12 +0800

    init

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -3,11 +3,13 @@ def call() {
 
     node{
         stage('Checkout') {
-            checkout scm
+          checkout([$class: 'GitSCM', branches: [[name: "main"]], extensions: [[$class: 'PruneStaleBranch']], gitTool: 'my_git', userRemoteConfigs: [[credentialsId: 'git-root', url: "http://172.10.32.130/ops/cicd.git"]]])
         }
-        //def cfg = pipelineCfg()
+
+        def cfg = pipelineCfg()
         //def currentDir = pwd()
         echo "${JOB_NAME}"
+
 
         switch(cfg.type) {
             case "python":
