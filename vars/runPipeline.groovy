69dddb12c843f5d27c4ac62ac1e0672754323af9
lisongtao <lisongtao@hrfax.cn>
2024-05-24 22:24:38 +0800

    init

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -9,7 +9,7 @@ def call() {
         def cfg = pipelineCfg()
         //def currentDir = pwd()
         echo "${JOB_NAME}"
-        echo ${cfg}
+        echo "${cfg}"
 
 
         switch(cfg.type) {
