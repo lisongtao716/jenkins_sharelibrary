4d8d36f82b7752b0e9b3786ac6687f829cad4b94
lisongtao <lisongtao@hrfax.cn>
2024-05-24 22:10:58 +0800

    init

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -3,7 +3,7 @@ def call() {
 
     node{
         stage('Checkout') {
-          checkout([$class: 'GitSCM', branches: [[name: "main"]], extensions: [[$class: 'PruneStaleBranch']], gitTool: 'my_git', userRemoteConfigs: [[credentialsId: 'git-root', url: "http://172.10.32.130/ops/cicd.git"]]])
+          checkout([$class: 'GitSCM', branches: [[name: "main"]], extensions: [[$class: 'PruneStaleBranch']], gitTool: 'my_git', userRemoteConfigs: [[credentialsId: 'git-root', url: "http://172.10.32.141/ops/cicd.git"]]])
         }
 
         def cfg = pipelineCfg()
